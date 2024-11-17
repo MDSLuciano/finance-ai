c3d31c6f9fe8c8ccbfefb1bee6ab11d82df09510
Luciano Machado <lucianoomachados@gmail.com>
2024-11-17 08:19:35 -0300

    feat: create add transaction dialog

---
+++ app/_constant/transaction.ts
@@ -0,0 +1,86 @@
+import { TransactionCategory, TransactionPaymentMethod, TransactionType } from "@prisma/client"
+
+export const TRANSACTION_CATEGORIES_LABELS = {
+  EDUCATION :"Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outro",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+}
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  BANK_TRANSFER: "Transferência Bancária",
+  BANK_SLIP: "Boleto Bancário",
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  OTHER: "Outro",
+  PIX: "PIX",
+}
+
+export const TRANSACTION_TYPES_OPTIONS = [{
+    value: TransactionType.DEPOSIT,
+    label: "Depósito",
+}, {
+    value: TransactionType.EXPENSE,
+    label: "Despesa",
+}, {
+    value: TransactionType.INVESTMENT,
+    label: "Investimento",
+}]
+
+export const PAYMENT_METHODS_OPTIONS = [{
+    value: TransactionPaymentMethod.PIX,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.PIX,
+}, {
+    value: TransactionPaymentMethod.BANK_TRANSFER,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.BANK_TRANSFER,
+}, {
+    value: TransactionPaymentMethod.BANK_SLIP,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.BANK_SLIP,
+}, {
+    value: TransactionPaymentMethod.CASH,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.CASH,
+}, {
+    value: TransactionPaymentMethod.CREDIT_CARD,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.CREDIT_CARD,
+}, {
+    value: TransactionPaymentMethod.DEBIT_CARD,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.DEBIT_CARD,
+},{
+    value: TransactionPaymentMethod.OTHER,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS.OTHER,
+}]
+
+export const TRANSACTION_CATEGORIES_OPTIONS = [{
+    value: TransactionCategory.EDUCATION, 
+    label: TRANSACTION_CATEGORIES_LABELS.EDUCATION,
+}, {
+    value: TransactionCategory.ENTERTAINMENT, 
+    label: TRANSACTION_CATEGORIES_LABELS.ENTERTAINMENT,
+}, {
+    value: TransactionCategory.FOOD, 
+    label: TRANSACTION_CATEGORIES_LABELS.FOOD,
+}, {
+    value: TransactionCategory.HEALTH, 
+    label: TRANSACTION_CATEGORIES_LABELS.HEALTH,
+}, {
+    value: TransactionCategory.HOUSING, 
+    label: TRANSACTION_CATEGORIES_LABELS.HOUSING,
+  }, {
+    value: TransactionCategory.SALARY, 
+    label: TRANSACTION_CATEGORIES_LABELS.SALARY,
+  }, {
+    value: TransactionCategory.TRANSPORTATION, 
+    label: TRANSACTION_CATEGORIES_LABELS.TRANSPORTATION,
+  }, {
+    value: TransactionCategory.UTILITY, 
+    label: TRANSACTION_CATEGORIES_LABELS.UTILITY,
+  }, {
+      value: TransactionCategory.OTHER, 
+      label: TRANSACTION_CATEGORIES_LABELS.OTHER,
+}]
--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,32 +1,12 @@
 "use client"
 
-import { Transaction, TransactionCategory } from "@prisma/client"
+import { Transaction } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import TransactionTypeBadge from "./_components/type-badge"
 import { Button } from "@/app/_components/ui/button"
 import { PencilIcon, TrashIcon } from "lucide-react"
+import { TRANSACTION_CATEGORIES_LABELS, TRANSACTION_PAYMENT_METHOD_LABELS } from "@/app/_constant/transaction"
 
-export const TRANSACTION_CATEGORIES_LABELS = {
-  [TransactionCategory.EDUCATION]: "Educação",
-  [TransactionCategory.ENTERTAINMENT]: "Entretenimento",
-  [TransactionCategory.FOOD]: "Alimentação",
-  [TransactionCategory.HEALTH]: "Saúde",
-  [TransactionCategory.HOUSING]: "Moradia",
-  [TransactionCategory.OTHER]: "Outro",
-  [TransactionCategory.SALARY]: "Salário",
-  [TransactionCategory.TRANSPORTATION]: "Transporte",
-  [TransactionCategory.UTILITY]: "Utilidades",
-}
-
-export const TRANSACTION_PAYMENT_METHOD_LABELS = {
-  BANK_TRANSFER: "Transferência Bancária",
-  BANK_SLIP: "Boleto Bancário",
-  CASH: "Dinheiro",
-  CREDIT_CARD: "Cartão de Crédito",
-  DEBIT_CARD: "Cartão de Débito",
-  OTHER: "Outro",
-  PIX: "PIX",
-}
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -36,25 +16,25 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
-    cell: ({ row: { original: transaction }}) => 
-      <TransactionTypeBadge transaction={transaction}/>
+    cell: ({ row: { original: transaction } }) =>
+      <TransactionTypeBadge transaction={transaction} />
   },
   {
     accessorKey: "category",
     header: "Categoria",
-    cell: ({ row: { original: transaction }}) => 
+    cell: ({ row: { original: transaction } }) =>
       TRANSACTION_CATEGORIES_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
-    cell: ({ row: { original: transaction }}) =>
+    cell: ({ row: { original: transaction } }) =>
       TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
-    cell: ({ row: { original: transaction }}) => 
+    cell: ({ row: { original: transaction } }) =>
       new Date(transaction.date).toLocaleDateString("pt-BR", {
         day: "2-digit",
         month: "short",
@@ -64,7 +44,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "amount",
     header: "Valor",
-    cell: ({ row: { original: transaction }}) => 
+    cell: ({ row: { original: transaction } }) =>
       new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
@@ -73,18 +53,18 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "actions",
     header: "Ações",
-    cell: () =>{
+    cell: () => {
       return (
-      <div className="space-x-2">
-        <Button variant="ghost" size="icon" className="text-muted-foreground">
-          <PencilIcon />
-        </Button>
-        <Button variant="ghost" size="icon" className="text-muted-foreground">
-          <TrashIcon />
-        </Button>
-      </div>
+        <div className="space-x-2">
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <PencilIcon />
+          </Button>
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+        </div>
       )
     }
   }
-  
+
 ]
