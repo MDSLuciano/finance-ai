b087624ed853af3b49811ebd9281d037c985cd5a
Luciano Machado <lucianoomachados@gmail.com>
2024-11-18 07:06:34 -0300

    feat: ensure transactions and subscriptions page are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -17,9 +17,6 @@ const TransactionsPage = async () => {
         where: {
             userId,
         },
-        orderBy: {
-            date: "desc",
-        },
     });
     return (
         <>
